19a0ba8021def6c7fb27ad48cb87c53d1346d019
Vitali Yanusheuski <butuk@users.noreply.github.com>
2022-05-09 01:03:53 +0400

    feat: add keyboard layout

---
+++ dist/main.js
@@ -0,0 +1,712 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/buttons.js":
+/*!************************!*\
+  !*** ./src/buttons.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+const BUTTONS = {
+
+  row_0: [
+    {
+      btnName: 'IntlBackslash',
+      type: 'symbol',
+      EN: '±',
+      en: '§',
+      BY: '+',
+      by: '’',
+    },
+
+    {
+      btnName: 'Digit1',
+      type: 'symbol',
+      EN: '!',
+      en: '1',
+      BY: '!',
+      by: '1',
+    },
+
+    {
+      btnName: 'Digit2',
+      type: 'symbol',
+      EN: '@',
+      en: '2',
+      BY: '"',
+      by: '2',
+    },
+
+    {
+      btnName: 'Digit3',
+      type: 'symbol',
+      EN: '#',
+      en: '3',
+      BY: '№',
+      by: '3',
+    },
+
+    {
+      btnName: 'Digit4',
+      type: 'symbol',
+      EN: '$',
+      en: '4',
+      BY: '%',
+      by: '4',
+    },
+
+    {
+      btnName: 'Digit5',
+      type: 'symbol',
+      EN: '%',
+      en: '5',
+      BY: ':',
+      by: '5',
+    },
+
+    {
+      btnName: 'Digit6',
+      type: 'symbol',
+      EN: '^',
+      en: '6',
+      BY: ',',
+      by: '6',
+    },
+
+    {
+      btnName: 'Digit7',
+      type: 'symbol',
+      EN: '&',
+      en: '7',
+      BY: '.',
+      by: '7',
+    },
+
+    {
+      btnName: 'Digit8',
+      type: 'symbol',
+      EN: '*',
+      en: '8',
+      BY: ';',
+      by: '8',
+    },
+
+    {
+      btnName: 'Digit9',
+      type: 'symbol',
+      EN: '(',
+      en: '9',
+      BY: '(',
+      by: '9',
+    },
+
+    {
+      btnName: 'Digit0',
+      type: 'symbol',
+      EN: ')',
+      en: '0',
+      BY: ')',
+      by: '0',
+    },
+
+    {
+      btnName: 'Minus',
+      type: 'symbol',
+      EN: '_',
+      en: '-',
+      BY: '_',
+      by: '-',
+    },
+
+    {
+      btnName: 'Equal',
+      type: 'symbol',
+      EN: '+',
+      en: '=',
+      BY: '+',
+      by: '=',
+    },
+
+    {
+      btnName: 'Backspace',
+      type: 'modifier',
+      symbol: '⌫',
+    },
+  ],
+
+  row_1: [
+    {
+      btnName: 'Tab',
+      type: 'modifier',
+      symbol: '↹',
+    },
+
+    {
+      btnName: 'KeyQ',
+      type: 'symbol',
+      EN: 'Q',
+      en: 'q',
+      BY: 'Й',
+      by: 'й',
+    },
+
+    {
+      btnName: 'KeyW',
+      type: 'symbol',
+      EN: 'W',
+      en: 'w',
+      BY: 'Ц',
+      by: 'ц',
+    },
+
+    {
+      btnName: 'KeyE',
+      type: 'symbol',
+      EN: 'E',
+      en: 'e',
+      BY: 'У',
+      by: 'у',
+    },
+
+    {
+      btnName: 'KeyR',
+      type: 'symbol',
+      EN: 'R',
+      en: 'r',
+      BY: 'К',
+      by: 'к',
+    },
+
+    {
+      btnName: 'KeyT',
+      type: 'symbol',
+      EN: 'T',
+      en: 't',
+      BY: 'Е',
+      by: 'е',
+    },
+
+    {
+      btnName: 'KeyY',
+      type: 'symbol',
+      EN: 'Y',
+      en: 'y',
+      BY: 'Н',
+      by: 'н',
+    },
+
+    {
+      btnName: 'KeyU',
+      type: 'symbol',
+      EN: 'U',
+      en: 'u',
+      BY: 'Г',
+      by: 'г',
+    },
+
+    {
+      btnName: 'KeyI',
+      type: 'symbol',
+      EN: 'I',
+      en: 'i',
+      BY: 'Ш',
+      by: 'ш',
+    },
+
+    {
+      btnName: 'KeyO',
+      type: 'symbol',
+      EN: 'O',
+      en: 'o',
+      BY: 'Ў',
+      by: 'ў',
+    },
+
+    {
+      btnName: 'KeyP',
+      type: 'symbol',
+      EN: 'P',
+      en: 'p',
+      BY: 'З',
+      by: 'з',
+    },
+
+    {
+      btnName: 'BracketLeft',
+      type: 'symbol',
+      EN: '{',
+      en: '[',
+      BY: 'Х',
+      by: 'х',
+    },
+
+    {
+      btnName: 'BracketRight',
+      type: 'symbol',
+      EN: '}',
+      en: ']',
+      BY: "'",
+      by: "'",
+    },
+
+    {
+      btnName: 'Backslash',
+      type: 'symbol',
+      EN: '|',
+      en: '\\',
+      BY: 'Ё',
+      by: 'ё',
+    },
+  ],
+
+  row_2: [
+    {
+      btnName: 'CapsLock',
+      type: 'modifier',
+      symbol: '⇪',
+    },
+
+    {
+      btnName: 'KeyA',
+      type: 'symbol',
+      EN: 'A',
+      en: 'a',
+      BY: 'Ф',
+      by: 'ф',
+    },
+
+    {
+      btnName: 'KeyS',
+      type: 'symbol',
+      EN: 'S',
+      en: 's',
+      BY: 'Ы',
+      by: 'ы',
+    },
+
+    {
+      btnName: 'KeyD',
+      type: 'symbol',
+      EN: 'D',
+      en: 'd',
+      BY: 'В',
+      by: 'в',
+    },
+
+    {
+      btnName: 'KeyF',
+      type: 'symbol',
+      EN: 'F',
+      en: 'f',
+      BY: 'А',
+      by: 'а',
+    },
+
+    {
+      btnName: 'KeyG',
+      type: 'symbol',
+      EN: 'G',
+      en: 'g',
+      BY: 'П',
+      by: 'п',
+    },
+
+    {
+      btnName: 'KeyH',
+      type: 'symbol',
+      EN: 'H',
+      en: 'h',
+      BY: 'Р',
+      by: 'р',
+    },
+
+    {
+      btnName: 'KeyJ',
+      type: 'symbol',
+      EN: 'J',
+      en: 'j',
+      BY: 'О',
+      by: 'о',
+    },
+
+    {
+      btnName: 'KeyK',
+      type: 'symbol',
+      EN: 'K',
+      en: 'k',
+      BY: 'Л',
+      by: 'л',
+    },
+
+    {
+      btnName: 'KeyL',
+      type: 'symbol',
+      EN: 'L',
+      en: 'l',
+      BY: 'Д',
+      by: 'д',
+    },
+
+    {
+      btnName: 'Semicolon',
+      type: 'symbol',
+      EN: ':',
+      en: ';',
+      BY: 'Ж',
+      by: 'ж',
+    },
+
+    {
+      btnName: 'Quote',
+      type: 'symbol',
+      EN: '"',
+      en: "'",
+      BY: 'Э',
+      by: 'э',
+    },
+
+    {
+      btnName: 'Enter',
+      type: 'modifier',
+      symbol: '↵',
+    },
+  ],
+
+  row_3: [
+    {
+      btnName: 'ShiftLeft',
+      type: 'modifier',
+      symbol: '⇧',
+    },
+
+    {
+      btnName: 'Backquote',
+      type: 'symbol',
+      EN: '~',
+      en: '`',
+      BY: '„',
+      by: '“',
+    },
+
+    {
+      btnName: 'KeyZ',
+      type: 'symbol',
+      EN: 'Z',
+      en: 'z',
+      BY: 'Я',
+      by: 'я',
+    },
+
+    {
+      btnName: 'KeyX',
+      type: 'symbol',
+      EN: 'X',
+      en: 'x',
+      BY: 'Ч',
+      by: 'ч',
+    },
+
+    {
+      btnName: 'KeyC',
+      type: 'symbol',
+      EN: 'C',
+      en: 'c',
+      BY: 'С',
+      by: 'с',
+    },
+
+    {
+      btnName: 'KeyV',
+      type: 'symbol',
+      EN: 'V',
+      en: 'v',
+      BY: 'М',
+      by: 'м',
+    },
+
+    {
+      btnName: 'KeyB',
+      type: 'symbol',
+      EN: 'B',
+      en: 'b',
+      BY: 'І',
+      by: 'і',
+    },
+
+    {
+      btnName: 'KeyN',
+      type: 'symbol',
+      EN: 'N',
+      en: 'n',
+      BY: 'Т',
+      by: 'т',
+    },
+
+    {
+      btnName: 'KeyM',
+      type: 'symbol',
+      EN: 'M',
+      en: 'm',
+      BY: 'Ь',
+      by: 'ь',
+    },
+
+    {
+      btnName: 'Comma',
+      type: 'symbol',
+      EN: '<',
+      en: ',',
+      BY: 'Б',
+      by: 'б',
+    },
+
+    {
+      btnName: 'Period',
+      type: 'symbol',
+      EN: '>',
+      en: '.',
+      BY: 'Ю',
+      by: 'ю',
+    },
+
+    {
+      btnName: 'Slash',
+      type: 'symbol',
+      EN: '?',
+      en: '/',
+      BY: '?',
+      by: '/',
+    },
+
+    {
+      btnName: 'ArrowUp',
+      type: 'modifier',
+      symbol: '↑',
+    },
+
+    {
+      btnName: 'ShiftRight',
+      type: 'modifier',
+      symbol: '⇧',
+    },
+  ],
+
+  row_4: [
+    {
+      btnName: 'Language',
+      type: 'modifier',
+      symbol: 'by',
+      alternative: 'en',
+    },
+    {
+      btnName: 'ControlLeft',
+      type: 'modifier',
+      symbol: '^',
+    },
+    {
+      btnName: 'AltLeft',
+      type: 'modifier',
+      symbol: '⌥',
+    },
+    {
+      btnName: 'MetaLeft',
+      type: 'modifier',
+      symbol: '⌘',
+    },
+    {
+      btnName: 'Space',
+      type: 'modifier',
+      symbol: '&nbsp;',
+    },
+    {
+      btnName: 'MetaRight',
+      type: 'modifier',
+      symbol: '⌘',
+    },
+    {
+      btnName: 'AltRight',
+      type: 'modifier',
+      symbol: '⌥',
+    },
+    {
+      btnName: 'ArrowLeft',
+      type: 'modifier',
+      symbol: '←',
+    },
+    {
+      btnName: 'ArrowDown',
+      type: 'modifier',
+      symbol: '↓',
+    },
+    {
+      btnName: 'ArrowRight',
+      type: 'modifier',
+      symbol: '→',
+    },
+  ],
+};
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (BUTTONS);
+
+
+/***/ }),
+
+/***/ "./src/create.js":
+/*!***********************!*\
+  !*** ./src/create.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ createSetting)
+/* harmony export */ });
+/* harmony import */ var _buttons_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./buttons.js */ "./src/buttons.js");
+// eslint-disable-next-line import/extensions
+
+
+function createSetting() {
+  const PLACE = document.createElement('div');
+  PLACE.className = 'container';
+  document.body.append(PLACE);
+
+  const HEADER = document.createElement('h1');
+  HEADER.innerHTML = 'Virtual MacOS keyboard';
+  PLACE.append(HEADER);
+
+  const TEXT_AREA = document.createElement('textarea');
+  // TEXT_AREA.setAttribute('disabled', 'true');
+  PLACE.append(TEXT_AREA);
+
+  const KEYBOARD = document.createElement('div');
+  KEYBOARD.className = 'keyboard';
+  PLACE.append(KEYBOARD);
+
+  // eslint-disable-next-line no-plusplus
+  function addBtnsToRow(rowNum) {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const button of _buttons_js__WEBPACK_IMPORTED_MODULE_0__["default"][`row_${rowNum}`]) {
+      const btn = document.createElement('button');
+      btn.className = 'button';
+      if (button.type === 'symbol') {
+        btn.classList.add('button_symbol');
+        btn.innerHTML = button.en;
+      } else {
+        btn.classList.add('button_modifier');
+        btn.innerHTML = button.symbol;
+      }
+      btn.id = button.btnName;
+
+      document.querySelectorAll('.keyboard__row')[rowNum].append(btn);
+    }
+  }
+
+  // eslint-disable-next-line no-plusplus,guard-for-in,no-restricted-syntax
+  for (const rw in _buttons_js__WEBPACK_IMPORTED_MODULE_0__["default"]) {
+    const row = document.createElement('div');
+    row.className = 'keyboard__row';
+    row.id = `${rw}`;
+    KEYBOARD.append(row);
+    addBtnsToRow(rw.slice(-1));
+  }
+
+  document.querySelector('#Backspace').classList.add('button_size_medium');
+  document.querySelector('#Tab').classList.add('button_size_medium');
+  document.querySelector('#CapsLock').classList.add('button_size_long');
+  document.querySelector('#Enter').classList.add('button_size_long');
+  document.querySelector('#ShiftLeft').classList.add('button_size_medium');
+  document.querySelector('#ArrowUp').classList.add('button_size_small');
+  document.querySelector('#ShiftRight').classList.add('button_size_small');
+  document.querySelector('#Language').classList.add('button_size_small');
+  document.querySelector('#ControlLeft').classList.add('button_size_small');
+  document.querySelector('#AltLeft').classList.add('button_size_small');
+  document.querySelector('#MetaLeft').classList.add('button_size_medium');
+  document.querySelector('#Space').classList.add('button_space');
+  document.querySelector('#MetaRight').classList.add('button_size_medium');
+  document.querySelector('#AltRight').classList.add('button_size_small');
+  document.querySelector('#ArrowLeft').classList.add('button_size_small');
+  document.querySelector('#ArrowDown').classList.add('button_size_small');
+  document.querySelector('#ArrowRight').classList.add('button_size_small');
+}
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _create__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./create */ "./src/create.js");
+
+
+(0,_create__WEBPACK_IMPORTED_MODULE_0__["default"])();
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
